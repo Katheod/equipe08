24ac8491d024540e3851747fb303c33cb464243c
Mathis Boivin <mathis.boivin.2@ulaval.ca>
2025-10-05 10:19:35 -0400

    Refactor Devoir1.py to add print statements for debugging and update function call parameters; modify figure creation for clarity

--- Devoir1.py
+++ Devoir1.py
@@ -39,7 +39,7 @@ plt.ylabel('||E_{n+1}||')
 plt.title("||E_{n+1}|| en fonction de ||E_n|| en échelle double-logarithmique")
 plt.legend()
 plt.grid()
-plt.show()
+
 
 
 # Trouver l'ordre de convergence.
@@ -55,10 +55,10 @@ r4 = ((mu+1) - sqrt((mu+1.0)*(mu-3.0))) / (2*mu)
 f = lambda x: -(pow(mu, 3)*pow(x, 4)) + 2*pow(mu, 3)*pow(x, 3) - pow(mu, 2)*(mu + 1)*pow(x, 2) + pow(mu, 2)*x
 
 # Récupérer le point fixes et la liste d'erreurs.
-r, e_n = pointsFixes(f, 0.8, 1000, 1e-10)
+r, e_n = pointsFixes(f, 0.5, 1000, 1e-10)
 
 #Créer la figure 1.
-plt.figure(1)
+plt.figure(3)
 plt.semilogy(range(len(e_n)), e_n)
 plt.xlabel('itérations')
 plt.ylabel('||E_n||')
@@ -71,7 +71,7 @@ e_n1= e_n[1:]
 e_n = e_n[:-1]
 
 #Créer la figure 2
-plt.figure(2)
+plt.figure(4)
 plt.loglog(e_n, e_n1, label="log(C) + p*log(||E_n||)")
 plt.xlabel('||E_n||')
 plt.ylabel('||E_{n+1}||')
